ad9827db44db106a289d681c1c47ce491b651751
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2024-07-05 16:18:20 +0530

    loader component me

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { apiConnector } from "../api/apiConnector";
 import "./Form.css";
+import { GridLoader } from "react-spinners";
 
 const Form = () => {
   const { register, handleSubmit, reset } = useForm();
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async (data) => {
-    console.log(data);
+    setLoading(true);
     const formData = {
       leaderName: data.leaderName,
       startUpName: data.startUpName,
@@ -16,126 +18,135 @@ const Form = () => {
       leaderPhoneSecondary: data.leaderPhoneSecondary,
       leaderMail: data.leaderMail,
       teammates: data.teammates,
-      idea:data.idea,
-      currentStage:data.currentStage,
-      websiteLink:data.websiteLink
+      idea: data.idea,
+      currentStage: data.currentStage,
+      websiteLink: data.websiteLink
     };
 
-    const result = await apiConnector(
-      "POST",
-      import.meta.env.VITE_BACKEND_BASE_ENDPOINT + "/submitForm",
-      formData,
-      {},
-      { params: formData }
-    );
-
-    reset();
-    alert("Submitted");
+    try {
+      const result = await apiConnector(
+        "POST",
+        import.meta.env.VITE_BACKEND_BASE_ENDPOINT + "/submitForm",
+        formData,
+        {},
+        { params: formData }
+      );
+      // Handle success
+    } catch (error) {
+      // Handle error
+    } finally {
+      reset();
+      setLoading(false);
+      alert("Submitted");
+    }
   };
 
   return (
-    <div className="outer">
-      <div className="typewriter">
-        <h2 className="head">Register Now!!</h2>
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          <p id="label-text">Startup Name:</p>
-          <input
-            type="text"
-            placeholder="The Next Microsoft!"
-            name="startUpName"
-            {...register("startUpName", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p>Idea Of The Startup</p>
-          <input
-            type="text"
-            name="idea"
-            placeholder="Idea"
-            {...register("idea", { required: true })}
-          />
-        </label>
-        <br/>
-        <label>
-          <p id="label-text">Leader Name:</p>
-          <input
-            type="text"
-            name="leaderName"
-            placeholder="The Next Bill Gates"
-            {...register("leaderName", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p id="label-text">Leader's Phone Number:</p>
-          <input
-            type="text"
-            name="leaderPhone"
-            placeholder="+91 ***** *****"
-            {...register("leaderPhone", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p id="label-text">Alternate Phone Number:</p>
-          <input
-            type="text"
-            placeholder="+91 ***** *****"
-            name="leaderPhoneSecondary"
-            {...register("leaderPhoneSecondary", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p id="label-text">E-Mail:</p>
-          <input
-            type="email"
-            name="leaderMail"
-            placeholder="beststartupever@innovation.in"
-            {...register("leaderMail", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p>Number Of Teammates</p>
-          <input
-            type="Number"
-            name="teammates"
-            min="2"
-            max="10"
-            placeholder="00"
-            {...register("teammates", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p>What is the current stage of your Startup</p>
-          <input
-            type="text"
-            name="how"
-            placeholder="Ideation/Market Researh/Execution"
-            {...register("currentStage", { required: true })}
-          />
-        </label>
-        <br />
-        <label>
-          <p>Link of your website/Social Media(if any)</p>
-          <input
-            type="text"
-            name="how"
-            placeholder="Social Media"
-            {...register("websiteLink",{required:false})}
-          />
-        </label>
+    <>
+   
+      <div className="outer">
+        <div className="typewriter">
+          <h2 className="head">Register Now!!</h2>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} >
+          <label>
+            <p id="label-text">Startup Name:</p>
+            <input
+              type="text"
+              placeholder="The Next Microsoft!"
+              name="startUpName"
+              {...register("startUpName", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p>Idea Of The Startup</p>
+            <input
+              type="text"
+              name="idea"
+              placeholder="Idea"
+              {...register("idea", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p id="label-text">Leader Name:</p>
+            <input
+              type="text"
+              name="leaderName"
+              placeholder="The Next Bill Gates"
+              {...register("leaderName", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p id="label-text">Leader's Phone Number:</p>
+            <input
+              type="text"
+              name="leaderPhone"
+              placeholder="+91 ***** *****"
+              {...register("leaderPhone", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p id="label-text">Alternate Phone Number:</p>
+            <input
+              type="text"
+              placeholder="+91 ***** *****"
+              name="leaderPhoneSecondary"
+              {...register("leaderPhoneSecondary", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p id="label-text">E-Mail:</p>
+            <input
+              type="email"
+              name="leaderMail"
+              placeholder="beststartupever@innovation.in"
+              {...register("leaderMail", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p>Number Of Teammates</p>
+            <input
+              type="Number"
+              name="teammates"
+              min="2"
+              max="10"
+              placeholder="00"
+              {...register("teammates", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p>What is the current stage of your Startup</p>
+            <input
+              type="text"
+              name="how"
+              placeholder="Ideation/Market Researh/Execution"
+              {...register("currentStage", { required: true })}
+            />
+          </label>
+          <br />
+          <label>
+            <p>Link of your website/Social Media(if any)</p>
+            <input
+              type="text"
+              name="how"
+              placeholder="Social Media"
+              {...register("websiteLink", { required: false })}
+            />
+          </label>
 
-        <button className="form-button" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+         { loading ? <GridLoader color="#ADDDBA"/>:<button className="form-button" type="submit">
+            Submit
+          </button>}
+        </form>
+      </div>
+    </>
   );
 };
 
